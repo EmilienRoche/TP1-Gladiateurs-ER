974ddcfc62b49216a5f1e4c9fc3a18d0df1623ad
202330289 <202330289@PO-24862>
2024-09-08 21:54:39 -0400

    HAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaAAAAAAAAAaaaaaaaaHHAAAAHAHAHAHAHHA

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -1,4 +1,5 @@
 package personnages;
+import java.util.Random;
 
 public class Personnage {
 
@@ -105,7 +106,10 @@ public class Personnage {
     private int attaqueCalcul() {
         // TODO : Retourner la valeur de l'attaque du personnage.
         // Cette valeur est trouvée aléatoirement et doit se situer entre ZÉRO et valeurMaxAttaque.
-        return 0;
+        Random rand = new Random();
+        int randomNbr = rand.nextInt(valeurMaxAttaque - 0 + 1) + 0;
+        
+        return randomNbr;
     }
 
     public void frapperPersonnage(Personnage personnageCible) {
